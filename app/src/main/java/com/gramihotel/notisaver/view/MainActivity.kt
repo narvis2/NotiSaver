14893fe5055f153fd1c8d58368faa00f09b878df
narvis2 <choeyeongjun@choeyeongjun-ui-MacBookPro.local>
2025-04-15 15:25:46 +0900

    필요한 Dependency 추가 및 Hilt 기초작업 + NotificationListenerService 기초 작업

--- app/src/main/java/com/gramihotel/notisaver/view/MainActivity.kt
+++ app/src/main/java/com/gramihotel/notisaver/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gramihotel.notisaver
+package com.gramihotel.notisaver.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.gramihotel.notisaver.ui.theme.NotiSaverTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
